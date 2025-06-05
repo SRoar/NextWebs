e3a6fb2e48f97906e7cf7782e8cfc9efe51b30e6
Shivaji <Shivaji.Raj@frogforce503.org>
2025-06-05 17:01:06 -0400

    Working with API

---
+++ app/api/ollama/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+ 
+export async function POST(req) {
+try {
+    const { prompt } = await req.json();
+    const res = await fetch('http://localhost:11434/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      
+    },
+
+    body: JSON.stringify({ model: 'llama3',
+        prompt,
+        stream: false,
+      }),
+  })
+ 
+  const data = await res.json()
+ 
+  return NextResponse.json(data)
+}
+  
+  catch (error) {
+      return NextResponse.json({ error: "Failed" }, { status: 500 });
+    }
+}
